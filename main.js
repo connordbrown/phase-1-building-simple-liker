4a3323119763ff17d87274cbf32ab8f1ab71e821
Connor <connordbrown@gmail.com>
2023-08-06 15:29:13 -0600

    completes lab

--- main.js
+++ main.js
@@ -4,8 +4,35 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+// grab heart glyph elements
+const hearts = document.querySelectorAll('.like-glyph');
+// create event listener that performs callback when any heart glyph is clicked
+for (let heart of hearts ) {
+  heart.addEventListener('click', serverRequest);
+}
 
+// perform server call; change heart glyph if successful, else show error modal
+function serverRequest(e) {
+  mimicServerCall()
+    .then(changeHeart(e.target))
+    .catch(function(error) {
+      const modalVis = document.querySelector('.hidden');
+      modalVis.className = 'visible'
+      document.querySelector('#modal h2').textContent = error;
+      setTimeout(() => modalVis.className = 'hidden', 3000)
+    })
+}
 
+// swap heart glyph color and content
+function changeHeart(heartIcon) {
+  if (heartIcon.textContent === FULL_HEART) {
+    heartIcon.classList.remove('activated-heart');
+    heartIcon.textContent = EMPTY_HEART;
+  } else {
+    heartIcon.classList.add('activated-heart');
+    heartIcon.textContent = FULL_HEART;
+  }
+}
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
